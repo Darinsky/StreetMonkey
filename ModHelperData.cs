057578d922810a44088f69b466a23ef95db74135
Darinsky <90986232+Darinsky@users.noreply.github.com>
2022-08-13 18:13:14 -0400

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -6,7 +6,7 @@ public static class ModHelperData
     public const string Version = "1.2.0";
     public const string Name = "Street Monkey";
 
-    public const string Description = "help me";
+    public const string Description = "Updated for the mod brower, remove paragon for now, since it was unpolished, and doesn't even work in the newest update. At least you got upc support";
 
     public const string RepoOwner = "Darinsky"; // TODO add your github username hero
     public const string RepoName = "StreetMonkey"; // TODO add your repo name here
